18bfbbd7db397bdfcb907327b686edf05752221b
Nick-roy <nroy08232@gmail.com>
2024-11-27 15:01:11 +0600

    Add first

---
+++ index.js
@@ -0,0 +1,9 @@
+var javascript = {
+    name : "javascript",
+    libraries :["React", "Angular","vue"],
+    printLibraries : function(){
+        this.libraries.forEach((a) =>     console.log(`${this.name} loves ${a}`
+        
+        ));
+    }}
+    javascript.printLibraries();
