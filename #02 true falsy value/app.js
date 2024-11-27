dbd2ad617ca3370203577549438ac6658601ac19
Nick-roy <nroy08232@gmail.com>
2024-11-27 21:02:04 +0600

    add file

---
+++ #02 true falsy value/app.js
@@ -0,0 +1,8 @@
+var myVar = ""; 
+// Replace 'null' with any value to test truthy or falsy
+// False, 0, "", Null, Undefined,
+if (myVar) {
+    console.log("I am truthy");
+} else {
+    console.log("I am falsy");
+}
