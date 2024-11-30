9b1003951ec643679d169064e8726ff9037eb7db
Nick-roy <nroy08232@gmail.com>
2024-11-30 22:13:57 +0600

    files

---
+++ #14 object tricks/app.js
@@ -0,0 +1,13 @@
+var user ={
+    name : "Javascript",
+    estd:1995,
+    inovator:"Brendon Eich"
+}
+
+var key = Object.keys(user);
+var value = Object.values(user)
+var entries = Object.entries(user)
+
+console.log(key)
+console.log(value)
+console.log(entries)
---
+++ #15 function/app.js
@@ -0,0 +1,5 @@
+function myfun(x = 10){
+    return x ;
+}
+
+console.log(myfun(null))
